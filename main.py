47eac810625a710484f96c8cbae88e743177c1a4
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-09-19 00:51:59 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+states = data["state"].to_list()
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state name?").title()
+    if answer_state == "Exit":
+        break
+    if answer_state in states:
+        state_cord = data[data.state == answer_state]
+        x_cord = state_cord.x
+        y_cord = state_cord.y
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(int(x_cord), int(y_cord))
+        t.write(answer_state)
+        guessed_states.append(answer_state)
+    else:
+        print("nothing yet")
+
+unguessed_states = []
+for i in states:
+    if i not in guessed_states:
+        unguessed_states.append(i)
+        ug_state = data[data.state == i]
+        tu = turtle.Turtle()
+        tu.hideturtle()
+        tu.color("red")
+        tu.penup()
+        tu.goto(x=int(ug_state.x), y=int(ug_state.y))
+        tu.write(i)
+        new_data = pandas.DataFrame(unguessed_states)
+        new_data.to_csv("States to Learn.csv")
+
